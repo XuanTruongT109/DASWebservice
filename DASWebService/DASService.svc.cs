b3dc0bb58005133c020ef413a04a94ff5a6f2a63
XuanTruongT109 <XuanTruongT109@gmail.com>
2018-03-07 12:30:41 +0800

    The second commit

---
+++ DASWebService/DASService.svc.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace DASWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DASService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select DASService.svc or DASService.svc.cs at the Solution Explorer and start debugging.
+    public class DASService : IDASService
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString);
+        public string token = "7FEBE4AD-9B44-406B-A756-F553E7F212AE";
+        public string InsertCus(Customer infoCus)
+        {
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            if(infoCus.Token == token)
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand("spr_INSERT_CUSTMSTTABLE", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("@ShiptoName", SqlDbType.VarChar).Value = infoCus.ShiptoName;
+                        cmd.Parameters.Add("@Cust_Stat", SqlDbType.VarChar).Value = infoCus.Cust_Stat;
+                        cmd.Parameters.Add("@SalesOrg", SqlDbType.VarChar).Value = infoCus.SalesOrg;
+                        cmd.Parameters.Add("@CustNo", SqlDbType.VarChar).Value = infoCus.CustNo;
+                        cmd.Parameters.Add("@Status", SqlDbType.Decimal).Value = infoCus.Status;
+                        int result = cmd.ExecuteNonQuery();
+                        if (result == 1)
+                        {
+                            return "Insert sucess to table CUSTMSTTABLE";
+                        }
+                        else
+                        {
+                            return "Fail to insert table CUSTMSTTABLE";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }else
+            {
+                return "Provide wrong token";
+            }
+            
+        }
+        public string InsertProHier(ProHier proHier)
+        {
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            if (proHier.Token == token)
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand("spr_INSERT_PRODHIERTABLE", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("@BrandCode", SqlDbType.VarChar).Value = proHier.BrandCode;
+                        cmd.Parameters.Add("@BrandName", SqlDbType.VarChar).Value = proHier.BrandName;
+                        cmd.Parameters.Add("@SubBrandCode", SqlDbType.VarChar).Value = proHier.SubBrandCode;
+                        cmd.Parameters.Add("@SubbrandName", SqlDbType.VarChar).Value = proHier.SubbrandName;
+                        cmd.Parameters.Add("@ProductID", SqlDbType.VarChar).Value = proHier.ProductID;
+                        cmd.Parameters.Add("@CompanyID", SqlDbType.VarChar).Value = proHier.CompanyID;
+                        cmd.Parameters.Add("@Status", SqlDbType.Decimal).Value = proHier.Status;
+                        int result = cmd.ExecuteNonQuery();
+                        if (result == 1)
+                        {
+                            return "Insert sucess to table PRODHIERTABLE";
+                        }
+                        else
+                        {
+                            return "Fail to insert table PRODHIERTABLE";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            else
+            {
+                return "Provide wrong token";
+            }
+        }
+        public DataSet GetCNtoSAP(string SAPToken)
+        {
+            if(SAPToken == token)
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand("spr_GetDataSAPPI", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataSet ds = new DataSet();
+                        da.Fill(ds);
+                        cmd.ExecuteNonQuery();
+                        return ds;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ DASWebService/IDASService.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DASWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDASService" in both code and config file together.
+    [ServiceContract]
+    public interface IDASService
+    {
+        [OperationContract]
+        string InsertCus(Customer infoCus);
+        [OperationContract]
+        string InsertProHier(ProHier proHier);
+        [OperationContract]
+        DataSet GetCNtoSAP(string token);
+    }
+    [DataContract]
+    public class Customer
+    {
+        string shiptoname = string.Empty;
+        string cust_stat = string.Empty;
+        string salesorg = string.Empty;
+        string custno = string.Empty;
+        string status = string.Empty;
+        string token = string.Empty;
+
+        [DataMember]
+        public string ShiptoName
+        {
+            get { return shiptoname; }
+            set { shiptoname = value; }
+        }
+
+        [DataMember]
+        public string Cust_Stat
+        {
+            get { return cust_stat; }
+            set { cust_stat = value; }
+        }
+        [DataMember]
+        public string SalesOrg
+        {
+            get { return salesorg; }
+            set { salesorg = value; }
+        }
+        [DataMember]
+        public string CustNo
+        {
+            get { return custno; }
+            set { custno = value; }
+        }
+        [DataMember]
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+        [DataMember]
+        public string Token
+        {
+            get { return token; }
+            set { token = value; }
+        }
+    }
+    public class ProHier
+    {
+        string brandcode = string.Empty;
+        string brandname = string.Empty;
+        string subbrandcode = string.Empty;
+        string subbrandname = string.Empty;
+        string proID = string.Empty;
+        string companyId = string.Empty;
+        string status = string.Empty;
+        string token = string.Empty;
+
+        [DataMember]
+        public string BrandCode
+        {
+            get { return brandcode; }
+            set { brandcode = value; }
+        }
+        [DataMember]
+        public string BrandName
+        {
+            get { return brandname; }
+            set { brandname = value; }
+        }
+        [DataMember]
+        public string SubBrandCode
+        {
+            get { return subbrandcode; }
+            set { subbrandcode = value; }
+        }
+        [DataMember]
+        public string SubbrandName
+        {
+            get { return subbrandname; }
+            set { subbrandname = value; }
+        }
+        [DataMember]
+        public string ProductID
+        {
+            get { return proID; }
+            set { proID = value; }
+        }
+        [DataMember]
+        public string CompanyID
+        {
+            get { return companyId; }
+            set { companyId = value; }
+        }
+        [DataMember]
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+        [DataMember]
+        public string Token
+        {
+            get { return token; }
+            set { token = value; }
+        }
+
+    }
+}
